fd7c00a7da584eade0bcfbb23959172f6d33dd0d
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-22 12:59:50 -0500

    created Users table

---
+++ models/user.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    username: DataTypes.STRING,
+    email: DataTypes.STRING,
+    birthday: DataTypes.DATE,
+    password: DataTypes.STRING,
+    age: DataTypes.INTEGER
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
