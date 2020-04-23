7f816442e484b2e7066a0b9fd6f7d756522bb80d
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-23 13:15:14 -0500

    persist username, still have to add routes etc so it can function properly. Still have to add form from bootstrap as well. Also need mustache library

--- app.js
+++ app.js
@@ -2,4 +2,15 @@ const pgp = require ('pg-promise')()
 const connectionString = 'postgres://localhost:5432/postgres'
 const db = pgp(connectionString)
 
-console.log(db)
+db.oneOrNone('SELECT id FROM user WHERE username = $1', [username])
+.then((user) => {
+    if(user) {
+        res.render('register', {message: 'Username is already taken'})
+    }
+    else{
+        db.one('INSERT INTO users(username,password) VALUES($1,$2)', [username,password])
+        .then(() => {
+            res.send('SUCCESS') //this send will change from SUCCESS to something else
+        })
+    }
+})
