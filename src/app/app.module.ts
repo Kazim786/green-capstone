e35836efe058c9e18294e69e7b7a2ca59f133d82
AlexFisher <efishentcoding@gmail.com>
2020-04-23 15:52:43 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
+  declarations: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
