acebf06cf67c2bf48a181fa91c2e5b7863fcc6f1
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-25 16:29:45 -0500

    added more notes

--- models/shoppinglist.js
+++ models/shoppinglist.js
@@ -8,6 +8,7 @@ module.exports = (sequelize, DataTypes) => {
       shoppingList.belongsTo(models.store, { foreignKey: 'id'});
       shoppingList.hasOne(models.user, { foreignKey: 'id'});
       //should i replace shoppingList (name of table) with itemName?
+      //havent ran the migration yet, will do it after confirmation
 
   };
   return shoppingList;
