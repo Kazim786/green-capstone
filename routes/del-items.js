31ae45cab7850f38c46883dbb9be41c125b22488
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-27 15:03:14 -0500

    fixed mistake

--- routes/del-items.js
+++ routes/del-items.js
@@ -2,7 +2,7 @@ const delrouter = require("express").Router()
 
 const models = require('../models')
 
-router.get("/del-items",(req, res) => {
+delrouter.get("/del-items",(req, res) => {
     models.shoppingLists.destroy('ItemName').then((data) => {
         res.json(data)
     }
